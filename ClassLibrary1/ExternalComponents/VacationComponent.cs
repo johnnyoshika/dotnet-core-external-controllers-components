f3b031d330864721f2f0b8372c95063447b00f46
Johnny Oshika <johnnyoshika@gmail.com>
2020-07-15 09:31:18 -0700

    Add VacationComponent

---
+++ ClassLibrary1/ExternalComponents/VacationComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.ExternalComponents
+{
+    public class VacationComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync() => Content("Vacation component");
+    }
+}
