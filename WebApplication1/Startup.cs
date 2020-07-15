c5d46c523920b0885b642cbe12f614aee23601ba
Johnny Oshika <johnnyoshika@gmail.com>
2020-07-15 10:36:22 -0700

    Load external view for view components

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using ClassLibrary1.ExternalComponents;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace WebApplication1
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            Env = env;
+        }
+
+        IHostingEnvironment Env { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -19,6 +30,17 @@ namespace WebApplication1
             services
                 .AddMvc()
                 .AddApplicationPart(Assembly.Load("ClassLibrary1"));
+
+            if (Env.IsDevelopment())
+                services.Configure<RazorViewEngineOptions>(options =>
+                {
+                    options.FileProviders.Add(new PhysicalFileProvider(Regex.Replace(Env.ContentRootPath, $"{Env.ApplicationName}", "ClassLibrary1")));
+                });
+            else
+                services.Configure<RazorViewEngineOptions>(options =>
+                {
+                    options.FileProviders.Add(new EmbeddedFileProvider(typeof(VacationComponent).GetTypeInfo().Assembly, "ClassLibrary1"));
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
